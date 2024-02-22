17a3e26420f11e08463f8d2d744284752a4cf0cc
Etan Joseph Heyman <etan@heyman.net>
2024-02-21 18:39:44 -0600

    crop client pics to square, fix flex problem

--- components/client-images/index.tsx
+++ components/client-images/index.tsx
@@ -4,11 +4,11 @@ import React from 'react';
 
 const ClientImages = () => {
   return (
-    <div className='flex h-fit w-full flex-wrap  justify-center gap-6 rounded-xl lg:pt-6'>
+    <div className='flex h-fit w-full grow-0 flex-wrap justify-center gap-6 rounded-xl lg:pt-6'>
       {clients.map((client) => (
         <div
           key={`${client.image_src}-${client.image_alt}`}
-          className='relative aspect-square w-[318px] rounded-xl lg:basis-1/3'
+          className='relative aspect-square size-[318px] lg:size-auto rounded-xl lg:basis-4/12'
         >
           <Image
             className='rounded-[75px] border-2 border-black object-contain'
